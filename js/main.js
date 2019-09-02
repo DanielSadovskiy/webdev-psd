1bf4f2db8d44b09c3fafc6ceb952b99ef411c57a
Daniel Sadovskiy <danielsadovskiy@gmail.com>
2019-09-03 00:34:25 +0300

    working

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(".button").click(function () {
+    if ($(this).attr("filter") && $(this).attr("filter") !== "all") {
+        $(".filter > div*[filter !='" + $(this).attr('filter') + "']").fadeOut(350);
+        $(".filter > div*[filter ='" + $(this).attr('filter') + "']").fadeIn(350);
+        $('.button').removeClass('active');
+        $(this).addClass("active");
+    } else {
+        $(".filter > div").fadeIn(350);
+        $('.button').removeClass('active');
+        $(this).addClass("active");
+    }
+});
