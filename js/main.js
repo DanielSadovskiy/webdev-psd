2fb1d6006ded8359d7a8c6d6fba3fb8239ca6ad9
Daniel Sadovskiy <danielsadovskiy@gmail.com>
2019-09-04 02:23:03 +0300

    90% psd to html without adaptive

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
-$(".button").click(function () {
-    if ($(this).attr("filter") && $(this).attr("filter") !== "all") {
-        $(".filter > div*[filter !='" + $(this).attr('filter') + "']").fadeOut(350);
-        $(".filter > div*[filter ='" + $(this).attr('filter') + "']").fadeIn(350);
-        $('.button').removeClass('active');
-        $(this).addClass("active");
-    } else {
-        $(".filter > div").fadeIn(350);
-        $('.button').removeClass('active');
-        $(this).addClass("active");
-    }
-});
+$(document).ready(function(){
+    $(".button").click(function () {
+        if ($(this).attr("filter") && $(this).attr("filter") !== "all") {
+            $(".filter > div*[filter !='" + $(this).attr('filter') + "']").fadeOut(350);
+            $(".filter > div*[filter ='" + $(this).attr('filter') + "']").fadeIn(350);
+            $('.button').removeClass('active');
+            $(this).addClass("active");
+        } else {
+            $(".filter > div").fadeIn(350);
+            $('.button').removeClass('active');
+            $(this).addClass("active");
+        }
+        
+    });
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows : false,
+        dots: true,
+        dotsClass: 'dots-style'
+      });
+})
